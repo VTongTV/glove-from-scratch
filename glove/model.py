e03810cc85b52d5e21a9bcb097ab1d6b3ca75aae
VTongTV <vedant.tong@gmail.com>
2024-10-14 19:01:14 +0530

    glove: add random parameter initialization for W, W_tilde, b, b_tilde

--- glove/model.py
+++ glove/model.py
@@ -9,3 +9,12 @@ class GloVeModel:
         self.W_tilde = None
         self.b = None
         self.b_tilde = None
+        self._init_params()
+
+    def _init_params(self):
+        rng = np.random.RandomState(config.SEED)
+        scale = 0.5 / self.embedding_dim
+        self.W = (rng.rand(self.vocab_size, self.embedding_dim) - 0.5) * 2 * scale
+        self.W_tilde = (rng.rand(self.vocab_size, self.embedding_dim) - 0.5) * 2 * scale
+        self.b = np.zeros(self.vocab_size)
+        self.b_tilde = np.zeros(self.vocab_size)
